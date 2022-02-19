1f4049dff5ce5231e99ea00b44f706f2a6d821c0
pro.boooooo <pro.boudjemline@gmail.com>
2022-02-19 02:43:01 +0100

    $

--- file_m.c
+++ file_m.c
@@ -4,17 +4,21 @@ int main(){
     system("CLS");
     system("color f0");
     SetConsoleTitle("Inacor.v0 by. Bilal");
-    printf("\t\t _____________________________________________________________\n");
-    printf("\t\t(_____________________________________________________________)\n\n");
-    printf("\t\t (_____)  |  ___ \x5c      /\x5c     (_______)   / ___ \x5c   (_____ \x5c \n");
-    printf("\t\t    _     | |   | |    /  \x5c     _         | |   | |   _____) )\n");
-    printf("\t\t   | |    | |   | |   / /\x5c \x5c   | |        | |   | |  (_____ ( \n");
-    printf("\t\t  _| |_   | |   | |  | |__| |  | |_____   | |___| |        | |\n");
-    printf("\t\t (_____)  |_|   |_|  |______|   \x5c______)   \x5c_____/         |_|\n");
-    printf("\t\t _____________________________________________________________\n");
-    printf("\t\t(_____________________________________________________________)\n");
-    printf("\t\t\t\t\t\t\t\t\t / \x5c      ._   _  ._   o   |_) o |  _. | \n");
-    printf("\t\t\t\t\t\t\t\t\t \x5c_/ \x5c/\x5c/ | | (/_ |    o   |_) | | (_| | \n\n");
+    printf("\t\t\t _____________________________________________________________\n");
+    printf("\t\t\t(_____________________________________________________________)\n\n");
+    printf("\t\t\t (_____)  |  ___ \x5c      /\x5c     (_______)   / ___ \x5c   (_____ \x5c \n");
+    printf("\t\t\t    _     | |   | |    /  \x5c     _         | |   | |   _____) )\n");
+    printf("\t\t\t   | |    | |   | |   / /\x5c \x5c   | |        | |   | |  (_____ ( \n");
+    printf("\t\t\t  _| |_   | |   | |  | |__| |  | |_____   | |___| |        | |\n");
+    printf("\t\t\t (_____)  |_|   |_|  |______|   \x5c______)   \x5c_____/         |_|\n");
+    printf("\t\t\t _____________________________________________________________\n");
+    printf("\t\t\t(_____________________________________________________________)\n");
+
+    printf("\t\t\t\t\t\t\t  ___ _ _           _     _ _     ____ _ ____ ___ \n");
+    printf("\t\t\t\t\t\t\t | _ |_) |__ _ __ _| |  _| | |_  |__  / |__  | __|\n");
+    printf("\t\t\t\t\t\t\t | _ \x5c | / _` / _` | | |_  .  _|   / /| | / /|__ \x5c\n");
+    printf("\t\t\t\t\t\t\t |___/_|_\x5c__,_\x5c__,_|_| |_     _|  /_/ |_|/_/ |___/\n");
+    printf("\t\t\t\t\t\t\t                         |_|_|                    \n\n");
 
     int inter_scan;
 
@@ -23,16 +27,16 @@ int main(){
         system("CLS");
         printf("[LOGGED WITH TEST LICENCE !]\n");
         printf("For somes problems, contact Bilaal#7175 !\n\n");
-        printf("[Interface 1 = TCP/IP]\n[Interface 2 = UDP]\n"); scanf("%d", &inter_scan);
+        printf("[Interface 1 = TCP/IP]\n[Interface 2 = UDP]\n\n"); scanf("%d", &inter_scan);
     }
 
     if(inter_scan == 1){
-        printf("opt. 1\n");
+        printf("\nopt. 1\n");
         return 0;
     }
 
     if(inter_scan == 2){
-        printf("opt. 2\n");
+        printf("\nopt. 2\n");
         return 0;
     }
 
